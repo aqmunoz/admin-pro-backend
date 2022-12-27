ade189ca037d9b1a1411780df2617f55ba1e2bae
Adniel Quintana <aqmunoz@gmail.com>
2022-12-26 22:00:00 -0500

    Fin sección 12

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcryptjs');
 
 const Usuario = require('../models/usuario');
 const { generarJWT } = require('../helpers/jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 const login = async (req, res = response) => {
 
@@ -49,6 +50,53 @@ const login = async (req, res = response) => {
 
 }
 
+const googleSignIn = async (req, res = response) => {
+
+    let googleToken = req.body.token;
+
+    try {
+
+        const { name, email,  picture} = await googleVerify(googleToken);
+
+        const usuarioDB = Usuario.findOne({ email });
+        let usuario;
+
+        if ( !usuarioDB ) {
+            usuario = new Usuario({
+                name,
+                email,
+                password: '@@',
+                img: picture,
+                google: true
+            });
+        } else {
+            usuario = usuarioDB;
+            usuario.google = true;
+        }
+
+        await usuario.save();
+
+        //Generar el token - JWT
+
+        const token = await generarJWT( usuario.id );
+
+        res.status(200).json({
+            ok: true,
+            token
+        });
+        
+    } catch (error) {
+        res.status(401).json({
+            ok: false,
+            msg: 'Token no es correcto'
+        });
+        
+    }
+
+    
+}
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
